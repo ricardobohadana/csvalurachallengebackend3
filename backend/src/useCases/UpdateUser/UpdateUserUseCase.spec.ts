594dd9cdeb84fab99d1aaa9f708406005f75c33c
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-05-01 07:10:57 -0300

    update user tests

--- backend/src/useCases/UpdateUser/UpdateUserUseCase.spec.ts
+++ backend/src/useCases/UpdateUser/UpdateUserUseCase.spec.ts
@@ -1,3 +1,97 @@
+import faker from "@faker-js/faker";
+import { updateUserUseCase } from ".";
+import { getError, NoErrorThrownError } from "../../helpers/NoErrorThrownError";
+import { sqliteUserRepository } from "../../repositories";
+
 describe("pass", () => {
-  expect(true).toBe(true);
+  it("should update a user accordingly", async () => {
+    const fakeName = faker.name.findName();
+    const fakeEmail = faker.internet.email();
+
+    const users = await sqliteUserRepository.getAll();
+    const user = users[0];
+    user.name = fakeName;
+    user.email = fakeEmail;
+
+    // update user
+    await updateUserUseCase.execute(user);
+
+    // verify that user was updated
+    const newUser = await sqliteUserRepository.getById(user.id);
+
+    expect(newUser.email).toBe(fakeEmail);
+    expect(newUser.name).toBe(fakeName);
+  });
+
+  it("should fail to update a user with already registered email", async () => {
+    const fakeName = faker.name.findName();
+
+    const users = await sqliteUserRepository.getAll();
+    const user = users[0];
+    user.name = fakeName;
+    user.email = "ricardobm@gmail.com";
+
+    // update user
+    const error = await getError<Error>(
+      async () => await updateUserUseCase.execute(user)
+    );
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error.message).toBe("O email inserido já está em uso");
+  });
+
+  it("should fail to update a user with empty email", async () => {
+    const fakeName = faker.name.findName();
+
+    const users = await sqliteUserRepository.getAll();
+    const user = users[0];
+    user.name = fakeName;
+    user.email = "";
+
+    // update user
+    const error = await getError<Error>(
+      async () => await updateUserUseCase.execute(user)
+    );
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error.message).toBe(
+      "Não é possível atualizar um usuário para um campo vazio"
+    );
+  });
+
+  it("should fail to update a user with empty name", async () => {
+    const fakeEmail = faker.internet.email();
+
+    const users = await sqliteUserRepository.getAll();
+    const user = users[0];
+    user.name = "";
+    user.email = fakeEmail;
+
+    // update user
+    const error = await getError<Error>(
+      async () => await updateUserUseCase.execute(user)
+    );
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error.message).toBe(
+      "Não é possível atualizar um usuário para um campo vazio"
+    );
+  });
+
+  it("should fail to update admin user", async () => {
+    const fakeEmail = faker.internet.email();
+    const fakeName = faker.name.findName();
+
+    const user = await sqliteUserRepository.findByEmail("admin@email.com.br");
+    user.name = fakeName;
+    user.email = fakeEmail;
+
+    // update user
+    const error = await getError<Error>(
+      async () => await updateUserUseCase.execute(user)
+    );
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error.message).toBe("Este usuário não pode ser atualizado");
+  });
 });
--- backend/src/useCases/UpdateUser/UpdateUserUseCase.ts
+++ backend/src/useCases/UpdateUser/UpdateUserUseCase.ts
@@ -7,12 +7,17 @@ class UpdateUserUseCase {
   async execute(data: IUpdateUserDTO) {
     if (data.email === "" || data.name === "")
       throw new Error(
-        "Não é possível atualizar um usuário para um campo vazio."
+        "Não é possível atualizar um usuário para um campo vazio"
       );
+
+    const newEmailUser = await this.usersRepository.findByEmail(data.email);
+
+    if (newEmailUser !== null && newEmailUser.id !== data.id)
+      throw new Error("O email inserido já está em uso");
 
     // impedindo que o admin seja atualizado
     if (data.id === "9a0d0a7f-695b-4231-81cb-d30f3b65ad05")
-      throw new Error("Este usuário não pode ser atualizado.");
+      throw new Error("Este usuário não pode ser atualizado");
 
     await this.usersRepository.update(data);
   }
