bf97c4c849d57e98a639bcffb7a725c70bcdb5f3
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-05-08 14:49:30 -0300

    deploy updates

--- frontend/src/global/index.ts
+++ frontend/src/global/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = "http://localhost:3333/api/v1";
+const apiBaseUrl = "https://backend.up.railway.app/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
