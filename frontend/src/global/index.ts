7f677a9b6df17cb71b00ab6402f7aac3328d6aec
Ricardo Bohadana Martins <cacolorde@gmail.com>
2022-05-09 23:26:38 -0300

    base url update

--- frontend/src/global/index.ts
+++ frontend/src/global/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const apiBaseUrl = "https://backend.up.railway.app/api/v1";
-const apiBaseUrl = "http://localhost:3333/api/v1";
+const apiBaseUrl = "https://backend.up.railway.app/api/v1";
+// const apiBaseUrl = "http://localhost:3333/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
